382f9d483c22c8408a0ca578b91e6c0ad5dd8dd5
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-04-25 14:35:30 +0300

    Add ViewComponent

---
+++ softShop/softShop/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace softShop.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var list = new List<string> { "Anasayfa", "Hakkımda", "İletişim" };
+            return View(list);
+        }
+    }
+}
