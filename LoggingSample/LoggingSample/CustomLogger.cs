8f7baba141e46329d64409bdbda4c9db33e50c7f
Türkay Ürkmez <turkay@turkayurkmez.com>
2022-04-28 16:05:47 +0300

    Customize Logging in .NET Core 6.0

---
+++ LoggingSample/LoggingSample/CustomLogger.cs
@@ -0,0 +1,81 @@
+﻿
+using System.Collections.Concurrent;
+
+namespace LoggingSample
+{
+    public class CustomLogger
+    {
+    }
+
+    public class ColoredConsoleLoggerConfiguration
+    {
+        public LogLevel LogLevel { get; set; } = LogLevel.Warning;
+        public int EventId { get; set; } = 0;
+        public ConsoleColor Color { get; set; } = ConsoleColor.Red;
+    }
+
+    public class ColoredConsoleLoggerProvider : ILoggerProvider
+    {
+        ColoredConsoleLoggerConfiguration config;
+        ConcurrentDictionary<string, ColoredConsoleLogger> loggers = new ConcurrentDictionary<string, ColoredConsoleLogger>();
+
+        public ColoredConsoleLoggerProvider(ColoredConsoleLoggerConfiguration configuration)
+        {
+            this.config = configuration;
+
+        }
+
+        public ILogger CreateLogger(string categoryName)
+        {
+            return loggers.GetOrAdd(categoryName, name => new ColoredConsoleLogger(name, config));
+
+        }
+
+        public void Dispose()
+        {
+           loggers.Clear();
+        }
+    }
+
+    public class ColoredConsoleLogger : ILogger
+    {
+        private readonly string name;
+        private readonly ColoredConsoleLoggerConfiguration configuration;
+
+        public ColoredConsoleLogger(string name, ColoredConsoleLoggerConfiguration configuration)
+        {
+            this.name = name;
+            this.configuration = configuration;
+        }
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel)
+        {
+            return logLevel == configuration.LogLevel;
+        }
+
+        private static object _lock = new object();
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+        {
+            if (!IsEnabled(logLevel))
+            {
+                return;
+            }
+            lock (_lock)
+            {
+                if (configuration.EventId == 0 || configuration.EventId == eventId.Id)
+                {
+                    var color = Console.ForegroundColor;
+                    Console.ForegroundColor = configuration.Color;
+                    Console.Write($"{logLevel} - {eventId.Id} - {name} - ");
+                    Console.Write($"{formatter(state, exception)}\n");
+                    Console.ForegroundColor = color;
+                }
+            }
+        }
+    }
+
+}
---
+++ LoggingSample/LoggingSample/Program.cs
@@ -0,0 +1,42 @@
+using LoggingSample;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
+builder.Logging.ClearProviders();
+
+var config = new ColoredConsoleLoggerConfiguration()
+{
+    LogLevel = LogLevel.Information,
+    Color = ConsoleColor.Green,
+
+};
+builder.Logging.AddProvider(new ColoredConsoleLoggerProvider(config));
+
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
